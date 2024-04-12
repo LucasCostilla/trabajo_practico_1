b7c30aba7320b32be0b76e047bbaa459ddf1a6d9
Win-10 <Win-10@DESKTOP-1HO0I44>
2024-04-12 16:32:46 -0300

    Ejercicios 1 y 2

--- TP1_CostillaLucas/src/ar/edu/unju/fi/ejercicio1/Main.java
+++ TP1_CostillaLucas/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -1,11 +1,18 @@
 package ar.edu.unju.fi.ejercicio1;
 
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Bienvenido a Programación Visual");
+		int variable1 = 8;
+		int variable2 = 5;
+		int variable3 = 1;
+		int variable4 = 20;
+		int variable5 = 9;
 
+double suma = (variable1 + variable2 + variable3 + variable4 + variable5);
+double promedio = suma / 5;
+
+System.out.println("El promedio es: " + promedio);
+	
+	
 	}
-
 }
---
+++ TP1_CostillaLucas/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,26 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+	public static void main(String[] args) {
+	String pais = "Argentina";
+	int edad = 38;
+	int alturaEdificio = 50;
+	String precioSuper = "$2500,00";
+	String telefono = "+543881234567";
+	double coseno = 0.87758256189037;
+	
+	System.out.println(
+			"País: "+pais);
+	System.out.println(
+			"Edad: "+edad);
+	System.out.println(
+			"Altura del edificio: "+alturaEdificio+" m");
+	System.out.println(
+			"El precio del producto es: "+precioSuper);
+	System.out.println(
+			"Teléfono: "+telefono);
+	System.out.println(
+			"El coseno de 0.5 es: "+coseno);
+	}
+	
+}
